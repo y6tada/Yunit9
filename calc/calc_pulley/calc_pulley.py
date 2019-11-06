42fc23473918f7548f3e47d20dde1dca6464aa80
y6tada <y6tada.cnc@gmail.com>
2019-11-06 15:08:49 +0900

    knee pulley graph fixed, feedback bug fixed, continue parameter tuning...

--- calc/calc_pulley/calc_pulley.py
+++ calc/calc_pulley/calc_pulley.py
@@ -8,30 +8,30 @@ import numpy as np
 import math as m
 
 # Leg kinematics design parameters
-min_leg_height_mm           = 50.0
-max_leg_height_mm           = 100.0
-leg_length                  = 125
-virtual_driver_radius_mm    = 95
-knee_pulley_radius_mm       = 14
-driver_init_radius_mm       = 5.0
-knee_driver_distance_mm     = 60
+h_min       = 50.0
+h_max       = 220.0
+l           = 125.0
+vdr         = 80.0
+rk          = 15.0
+rd_init     = 4.0
+f           = 30.0
 # Calculation parameter
-leg_height_step_mm          = 5.0
-# Servo motor specification parameters
+hh_init     = 2.5
+# Servo motor specification & leg parameters
 motor_stall_torque_Nm       = 8.9
 motor_noload_rpm            = 66
-# Fix calculation based parameters
-max_apply_force_N           = motor_stall_torque_Nm / virtual_driver_radius_mm / 10E3
+max_apply_force_N           = motor_stall_torque_Nm / vdr / 10E3
 max_apply_force_kgf         = max_apply_force_N / 9.81
-max_extention_speed_mms     = motor_noload_rpm * virtual_driver_radius_mm * m.pi / 30
+max_extention_speed_mms     = motor_noload_rpm * vdr * m.pi / 30
 max_extention_time_ms       = 250 / max_extention_speed_mms * 10E3
-driver_angle_range_deg      = (max_leg_height_mm - min_leg_height_mm) / virtual_driver_radius_mm
+driver_angle_range_deg      = (h_max - h_min) / vdr
 # Const
-r_k = knee_pulley_radius_mm
-vdr = virtual_driver_radius_mm
-l   = leg_length
-f   = knee_driver_distance_mm
-dd  = leg_height_step_mm / virtual_driver_radius_mm * -1
+ddar        = hh_init / vdr
+num_loop    = int((h_max - h_min) / hh_init) + 1
+# Visual
+visual_value        = 1
+visual_wire         = 0
+visual_controller   = 1
 
 
 def coordinate(axes, range_x, range_y, grid = True, xyline = True, xlabel = "x", ylabel = "y"):
@@ -61,8 +61,8 @@ def getSq(value1, value2):
     return m.sqrt( m.pow(value1, 2) + m.pow(value2, 2) )
 
 
-def getRotP_dd(_p):
-    rot_v   = np.array( [[np.cos(dd), -np.sin(dd)], [np.sin(dd),  np.cos(dd)]] )
+def getRotP(_p, _rot):
+    rot_v   = np.array( [[np.cos(_rot), -np.sin(_rot)], [np.sin(_rot),  np.cos(_rot)]] )
     return np.dot( rot_v, _p )
 
 
@@ -70,95 +70,119 @@ def get_psi(_h):
     return np.arcsin(_h / (2*l))
 
 
-def get_g(_rd):
-    return np.arcsin( (r_k - _rd) / f )
+def get_gar(_rd):
+    return np.arcsin( (rk - _rd) / f )
 
 
 def get_P(_rd):
-    return _rd * np.sin( get_g(_rd) ), _rd * np.cos( get_g(_rd) )
+    return _rd * np.sin( get_gar(_rd) ), _rd * np.cos( get_gar(_rd) )
+
+def get_t(_rd):
+    t_sq = m.pow(f, 2) - m.pow(rk - _rd, 2)
+    if t_sq >= 0:
+        return m.sqrt( t_sq )
+    else:
+        print('error t_sq < 0')
+
+def get_y(_rd):
+    return rk * get_gar(_rd) + get_t(_rd)
 
 
-def get_W(_rd):
-    return r_k * get_g(_rd) + getSq( f, (_rd - r_k) )
-
-
-def get_ctrl_rd(prev_rd, ref, gain, error=10E-6):
-    # Previous p を1ステップ回転させる
-    prev_p = [prev_rd * np.sin( get_g(prev_rd) ), prev_rd * np.cos( get_g(prev_rd) )]
-    prev_p = getRotP_dd(prev_p)
-    # フィードバック
-    crtl_rd = prev_rd
+def searchCurrentRd(_rd, _ddar, _d_xt, gain = 0.2, pass_error = 10E-5):
+    # Val for visual
+    global cnt, vis_t, vis_ref, vis_cur, vis_rd
+    # Var for feedback calculation
+    y0 = get_y(_rd)
+    P0 = getRotP(get_P(_rd), _ddar * -1)
+    P  = get_P(_rd)
     while 1 :
-        cur_w = get_W(crtl_rd)
-        cur_p = [ crtl_rd * np.sin(get_g(crtl_rd)), crtl_rd * np.cos(get_g(crtl_rd)) ]
-        cur_u = np.linalg.norm(cur_p - prev_p)
-        cur_error = ref - (cur_w + cur_u)
-        crtl_rd += cur_error * gain
-        print('y:%.3f\tref:%.3f\te:%.3f\tcur_u:%.3f' % (cur_w + cur_u, ref, cur_error, cur_u))
-        # print('ref:%.3f' % ref)
-        # print('cur_u:%.3f' % cur_u)
-
-        if m.fabs(cur_error) < error :
-            print('braek')
+        u   = np.linalg.norm(P - P0)
+        e   = (y0 + _d_xt) - (get_y(_rd) + u)
+        _rd += e * gain
+        P = get_P(_rd)
+        if visual_controller:
+            vis_t.append(cnt)
+            vis_ref.append(y0 + _d_xt)
+            vis_cur.append(get_y(_rd) + u)
+            vis_rd.append(_rd)
+            cnt += 1
+        if m.fabs(e) < pass_error:
             break
-
-    return crtl_rd
-
-
-def visual():
-    # fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-
-    # zero    = np.array([0, 0])
-    # P_rot   = getRotP_dd(P)
-
-    # plotVector(ax, P, P_rot - P, 6, color="green")
-    # plotVector(ax, zero, P, 6, color="red")
-    # plotVector(ax, zero, P_rot, 6, color="blue")
-
-    # np.set_printoptions(precision=2, floatmode='fixed')
-    # print('h\t',h)
-    # print('psi\t',psi)
-    # print('X\t',X)
-    # print('W\t',W)
-    # print('d\t',d)
-    # print('\np\n',p)
-    
-    plt.show()
-
+    return _rd
 
 if __name__ == '__main__':
-    # Val
-    r_d = driver_init_radius_mm
-    # Array
-    h   = np.array( [min_leg_height_mm], dtype=float )      # Distance between upper pitch axis and lower pitch axis [mm]
-    d   = np.array( [0], dtype=float )                      # Angle of driver [rad]
-    psi = np.array( [get_psi(h[0])], dtype=float )          # Knee inside angle [rad]
+    # Leg entire structure
+    h    = np.array( [h_min], dtype=float )     # Distance between upper pitch axis and lower pitch axis [mm]
+    dar  = np.array( [0], dtype=float )                     # Angle of driver [rad]
+    psi  = np.array( [get_psi(h[0])], dtype=float )         # Knee inside angle [rad]
+    d_xt = np.array( [0], dtype=float )                     # Wire extension length diff
+    # Driver pulley
+    rd   = np.array( [rd_init], dtype=float ) # Driver radius
+    gar  = np.array( [get_gar(rd)], dtype=float)            # Wire gradient
+    P    = np.array( [get_P(rd)], dtype=float)              # Driver pulley action point
+    # Var for visual
+    cnt     = 0
+    vis_t   = []
+    vis_ref = []
+    vis_cur = []
+    vis_rd  = []
     
-    g   = np.array( [get_g(r_d)], dtype=float)
-    w   = np.array( [get_W(r_d)], dtype=float )             # wire length through knee pulley and air              
-    P   = np.array( [get_P(r_d)], dtype=float)              # Driver pulley plot point
+    for n in range(1, num_loop):
+        h    = np.append( h, h[n-1] + hh_init )
+        dar  = np.append( dar, dar[n-1] + ddar )
+        psi  = np.append( psi, get_psi(h[n]) )
+        d_xt = np.append( d_xt, rk * (psi[n] - psi[n-1]) )
+        # Driver pulley
+        rd   = np.append( rd, searchCurrentRd(rd[n-1], ddar, d_xt[n]) )
+        gar  = np.append( gar, get_gar(rd[n]) )
+        P    = np.append( P, [get_P(rd[n])] )
+        # Visual wire
 
-    for n in range(1, int((max_leg_height_mm - min_leg_height_mm) / leg_height_step_mm)):
-        h   = np.append( h  , h[n-1] + leg_height_step_mm )
-        psi = np.append( psi, np.arcsin(h[n] / (2*l)) )
-        d   = np.append( d  , [(h[n] - min_leg_height_mm) / vdr] )
+    if visual_value:
+        print('dr range:%.1f' % m.degrees(driver_angle_range_deg))
+        print('h\tdar\tpsi\td_xt\tgar\trd')
+        for n in range(0, num_loop):
+            print('%.1f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t' % (h[n], dar[n], psi[n], d_xt[n], gar[n], rd[n]))
+
+    if visual_controller:
+        plt.figure(figsize=(6,12))
+
+        plt.subplot(311)
+        plt.plot(vis_t, vis_ref, label='ref')
+        plt.plot(vis_t, vis_cur, label='cur')
+        plt.title('Feedback')
+        plt.legend()
+
+        plt.subplot(312)
+        plt.plot(vis_t, vis_rd, label='rd')
+        plt.title('Rd')
+        plt.legend()
+
+        # Knee vector visual init
+        ax_wire  = plt.subplot(313)
+        plt.title('Knee coordinate')
+        coordinate(ax_wire, [-20, 80], [-25, 25])
+
+        for n in range(0, num_loop):
+            wedge = pat.Wedge(center=(0, 0), r=rk, theta1=90 - m.degrees(gar[n]), theta2=90, fc="white", ec="red")
+            ax_wire.add_patch( wedge )
+            ax_wire.plot( [rk * np.sin(gar[n]),f + rd[n] * np.sin(gar[n])], [rk * np.cos(gar[n]), rd[n] * np.cos(gar[n])] )
+            ax_wire.plot( [f, f + rd[n] * np.sin(gar[n])], [0, rd[n] * np.cos(gar[n])] )
+
+        plt.figure(figsize=(6,6))
+        ax2 = plt.subplot(111)
+        coordinate(ax2, [-15, 15], [-15, 15])
+        plt.title('Driver pulley')
+        plt.plot([0, rd[0] * np.sin(gar[0] - dar[0])], [0, rd[0] * np.cos(gar[0] - dar[0])], color='black')
+        for n in range(1, num_loop):
+            plt.plot([0, rd[n] * np.sin(gar[n] - dar[n])], [0, rd[n] * np.cos(gar[n] - dar[n])], color='blue')
+            dif_x = [rd[n-1] * np.sin(gar[n-1] - dar[n-1]), rd[n] * np.sin(gar[n] - dar[n])]
+            dif_y = [rd[n-1] * np.cos(gar[n-1] - dar[n-1]), rd[n] * np.cos(gar[n] - dar[n])]
+            plt.plot(dif_x, dif_y, color='black')
+        plt.plot([rd[n] * np.sin(gar[n] - dar[n]), 0], [rd[n] * np.cos(gar[n] - dar[n]),0], color='black')
+
+        plt.show()
+
+    # if visual_wire:
         
-        xx  = r_k * (psi[n] - psi[n-1])
-        print('xx:%.3f' % xx)
-        wx  = w[n-1] + r_k * (psi[n] - psi[n-1])
-        r_d = get_ctrl_rd(r_d, wx, -1.0)
-        w   = np.append( w  , [get_W(r_d)] )
-        P   = np.append( P  , [get_P(r_d)] )
-
-    # Visual
-    fig = plt.figure(figsize=(10,5))
-    plt.title('Knee coordinate')
-    ax = plt.subplot(111)
-    coordinate(ax, [-20, 80], [-25, 25])
-
-    knee_pulley_wire = pat.Wedge(center = (0, 0), r = r_k, theta1 = 90 - m.degrees(g), theta2 = 90, fc = "white", ec="red")
-    ax.add_patch(knee_pulley_wire)
-    ax.plot( [r_k * m.sin(g),f + r_d * m.sin(g)], [r_k * m.cos(g), r_d * m.cos(g)] )
-    ax.plot( [f, f + r_d * m.sin(g)], [0, r_d * m.cos(g)] )
-
-    plt.show()
+        # plt.show()
